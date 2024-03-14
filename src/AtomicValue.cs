583f8b5565627d57334038d398cf7dc0a63575ab
DarkCompet <darkcompet@gmail.com>
2024-03-14 21:18:30 +0700

    Add this keyword

--- src/AtomicValue.cs
+++ src/AtomicValue.cs
@@ -6,48 +6,48 @@ public class DkAtomicBool {
 	/// True: != 0, False: == 0
 	private long rawValue;
 
-	public bool value => Interlocked.Read(ref rawValue) != 0;
+	public bool value => Interlocked.Read(ref this.rawValue) != 0;
 
 	public bool Set(bool value) {
 		if (value) {
-			return Interlocked.Or(ref rawValue, 1) != 0;
+			return Interlocked.Or(ref this.rawValue, 1) != 0;
 		}
-		return Interlocked.And(ref rawValue, 0) == 0;
+		return Interlocked.And(ref this.rawValue, 0) == 0;
 	}
 }
 
 public class DkAtomicInt {
 	private long rawValue;
 
-	public int value => (int)Interlocked.Read(ref rawValue);
+	public int value => (int)Interlocked.Read(ref this.rawValue);
 
 	public int Increment() {
-		return (int)Interlocked.Increment(ref rawValue);
+		return (int)Interlocked.Increment(ref this.rawValue);
 	}
 
 	public int Decrement() {
-		return (int)Interlocked.Decrement(ref rawValue);
+		return (int)Interlocked.Decrement(ref this.rawValue);
 	}
 
 	public int Add(int more) {
-		return (int)Interlocked.Add(ref rawValue, more);
+		return (int)Interlocked.Add(ref this.rawValue, more);
 	}
 }
 
 public class DkAtomicLong {
 	private long rawValue;
 
-	public long value => Interlocked.Read(ref rawValue);
+	public long value => Interlocked.Read(ref this.rawValue);
 
 	public long Increment() {
-		return Interlocked.Increment(ref rawValue);
+		return Interlocked.Increment(ref this.rawValue);
 	}
 
 	public long Decrement() {
-		return Interlocked.Decrement(ref rawValue);
+		return Interlocked.Decrement(ref this.rawValue);
 	}
 
 	public long Add(long more) {
-		return Interlocked.Add(ref rawValue, more);
+		return Interlocked.Add(ref this.rawValue, more);
 	}
 }
